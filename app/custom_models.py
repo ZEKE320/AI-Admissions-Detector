b16ebaed4215424c81648aa6e90fe2cfebc89116
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2025-11-30 19:14:39 +0900

    ♻️ Refactor model classes in `custom_models.py` to improve readability and consistency, including renaming configuration classes and updating model initialization syntax.

--- app/custom_models.py
+++ app/custom_models.py
@@ -1,87 +1,108 @@
 from typing import Optional
-from transformers import PreTrainedModel, PretrainedConfig, DistilBertModel, BertModel
+
 import torch
 from torch import nn
+from transformers import BertModel, DistilBertModel, PretrainedConfig, PreTrainedModel
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class TransformerBasedModelDistilBert(nn.Module):
     def __init__(self):
         super(TransformerBasedModelDistilBert, self).__init__()
-        self.bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
+        self.bert = DistilBertModel.from_pretrained("distilbert-base-uncased")
         self.dropout = nn.Dropout(0.55)
         self.fc = nn.Linear(768, 2)
 
-    def forward(self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
+    def forward(
+        self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None
+    ):
         input_shape = input_ids.size()
         if attention_mask is None:
             attention_mask = torch.ones(input_shape, device=device)
-            
+
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
         pooled_output = outputs.last_hidden_state[:, 0, :]
         pooled_output = self.dropout(pooled_output)
         logits = self.fc(pooled_output)
         return logits
 
+
 class TransformerBasedModelBert(nn.Module):
     def __init__(self):
         super(TransformerBasedModelBert, self).__init__()
-        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
         self.dropout = nn.Dropout(0.55)
         self.fc = nn.Linear(768, 2)
 
-    def forward(self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
+    def forward(
+        self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None
+    ):
         input_shape = input_ids.size()
         if attention_mask is None:
             attention_mask = torch.ones(input_shape, device=device)
-            
+
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
         pooled_output = outputs[1]
         pooled_output = self.dropout(pooled_output)
         logits = self.fc(pooled_output)
         return logits
 
-class MyConfigDistil(PretrainedConfig):
+
+class MyConfigDistilBert(PretrainedConfig):
     model_type = "distilbert"
+
     def __init__(self, final_dropout=0.55, **kwargs):
         super().__init__(**kwargs)
         self.final_dropout = final_dropout
 
-class MyConfig(PretrainedConfig):
+
+class MyConfigBert(PretrainedConfig):
     model_type = "bert"
+
     def __init__(self, final_dropout=0.55, **kwargs):
         super().__init__(**kwargs)
         self.final_dropout = final_dropout
-        
+
+
 class MyHFModel_DistilBertBased(PreTrainedModel):
-    config_class = MyConfigDistil
+    config_class = MyConfigDistilBert
+
     def __init__(self, config):
         super().__init__(config)
         self.config = config
         self.model = TransformerBasedModelDistilBert()
-    def forward(self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
+
+    def forward(
+        self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None
+    ):
         input_shape = input_ids.size()
         if attention_mask is None:
             attention_mask = torch.ones(input_shape, device=device)
 
         return self.model(input_ids=input_ids, attention_mask=attention_mask)
 
+
 class MyHFModel_BertBased(PreTrainedModel):
-    config_class = MyConfig
+    config_class = MyConfigBert
+
     def __init__(self, config):
         super().__init__(config)
         self.config = config
         self.model = TransformerBasedModelBert()
-    def forward(self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None):
+
+    def forward(
+        self, input_ids: torch.Tensor, attention_mask: Optional[torch.Tensor] = None
+    ):
         input_shape = input_ids.size()
         if attention_mask is None:
             attention_mask = torch.ones(input_shape, device=device)
 
-        return self.model(input_ids=input_ids, attention_mask=attention_mask) 
+        return self.model(input_ids=input_ids, attention_mask=attention_mask)
 
-config = MyConfigDistil(0.55)
+
+config = MyConfigDistilBert(0.55)
 HF_DistilBertBasedModelAppDocs = MyHFModel_DistilBertBased(config)
 
-config_db = MyConfig(0.55)
+config_db = MyConfigBert(0.55)
 HF_BertBasedModelAppDocs = MyHFModel_BertBased(config_db)
